a90289938d9e96f2ba368f4e81b62a259c0368c9
Kai Jellinghaus <contact@kaij.tech>
2022-03-11 12:10:41 +0100

    Add URQL

---
+++ components/MainLayout.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from 'react';
+import { Provider } from 'urql';
+import urql from '../urql';
+
+const client = urql.createClient();
+
+type Props = {
+    children: ReactNode;
+};
+
+export default function Layout({ children } : Props) {
+    return (
+      <>
+        <Provider value={client}>
+            <main>{children}</main>
+        </Provider>
+      </>
+    )
+  }
--- pages/summoner/[summonerName]/index.tsx
+++ pages/summoner/[summonerName]/index.tsx
@@ -1,11 +1,19 @@
 import { NextPage } from 'next'
 import { useRouter } from 'next/router'
+import { Query } from 'urql'
 
 const Summoner : NextPage = () => {
   const router = useRouter()
   const { summonerName } = router.query
 
-  return <p>Summoner Name: {summonerName}</p>
+  return <>
+        <p>Summoner Name: {summonerName}</p>
+        <Query query={ "{ query { test } }" } variables={ "$summonerName = " }>{
+        (data) => 
+            <p>{JSON.stringify(data)}</p>
+        }
+        </Query>
+    </>
 }
 
 export default Summoner;
