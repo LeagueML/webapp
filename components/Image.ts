05be818d44f5acad8787277b1dcfb07ff3af5173
Kai Jellinghaus <contact@kaij.tech>
2022-03-11 20:57:08 +0100

    Cleanup

--- components/Image.ts
+++ components/Image.ts
@@ -6,6 +6,20 @@ const customLoader = ({ src } : { src : any}) => {
   return src
 }
 
+const normalizeSrc = src => {
+  return src.startsWith('/') ? src.slice(1) : src;
+};
+
+const cloudflareLoader = ({ src, width, quality }) => {
+  const params = [`width=${width}`];
+  if (quality) {
+    params.push(`quality=${quality}`);
+  }
+  const paramsString = params.join(',');
+  return `/cdn-cgi/image/${paramsString}/${normalizeSrc(src)}`;
+};
+
+
 export default function ImageP(props : NextPageContext) {
   return (
     <Image
--- next.config.js
+++ next.config.js
@@ -1,17 +1,3 @@
-const normalizeSrc = src => {
-  return src.startsWith('/') ? src.slice(1) : src;
-};
-
-const cloudflareLoader = ({ src, width, quality }) => {
-  const params = [`width=${width}`];
-  if (quality) {
-    params.push(`quality=${quality}`);
-  }
-  const paramsString = params.join(',');
-  return `/cdn-cgi/image/${paramsString}/${normalizeSrc(src)}`;
-};
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
