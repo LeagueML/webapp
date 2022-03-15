64f5afbe40707ecb19e6cf2254a4ad59318c160f
Kai Jellinghaus <contact@kaij.tech>
2022-03-15 19:03:08 +0100

    Update to latest

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Button>Test Button</Button>
+      <Button text='test' />
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
