f44d6d4bcbed6586b4c1a9c108a29f2a3adfa9c3
Kai Jellinghaus <contact@kaij.tech>
2022-03-19 23:21:19 +0100

    Update to latest package

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,23 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import Router from 'next/router';
+import type { NextPage } from "next";
+import Head from "next/head";
+import Router from "next/router";
 
-import styles from '../styles/Home.module.css';
+import styles from "../styles/Home.module.css";
 
-import { SummonerSearch } from '@league.ml/component-library';
-import { BsGithub } from 'react-icons/bs'
-import { GrGraphQl } from 'react-icons/gr'
-import { SiStorybook } from 'react-icons/si'
+import { SummonerSearch } from "@league.ml/component-library";
+import { BsGithub } from "react-icons/bs";
+import { GrGraphQl } from "react-icons/gr";
+import { SiStorybook } from "react-icons/si";
 
-import platforms from '../platforms';
+import platforms from "../platforms";
 
 const Home: NextPage = () => {
   return (
     <div>
-      <div className={ styles.header }>
-        <span className={ styles.title }>LEAGUE.ML</span>
+      <div className={styles.header}>
+        <span className={styles.title}>LEAGUE.ML</span>
       </div>
-      <div className={ styles.main }>
+      <div className={styles.main}>
         <Head>
           <title>LEAGUE.ML</title>
           <link rel="icon" href="/favicon.ico" />
@@ -25,49 +25,51 @@ const Home: NextPage = () => {
         <SummonerSearch
           initialPlatform={platforms[0]}
           platforms={platforms}
-          onSearch={ (name, platform) => Router.push(`/summoner/${platform.short.toLowerCase()}/${name}`) }
+          onSearch={(name, platform) =>
+            Router.push(`/summoner/${platform.short.toLowerCase()}/${name}`)
+          }
         />
       </div>
-      <div className={ styles.firstFooter }>
-        <div className={ styles.linkContainer }>
-          <a 
-            className={ styles.linkBadge } 
-            href='https://github.com/LeagueML' 
-            target='_blank'
+      <div className={styles.firstFooter}>
+        <div className={styles.linkContainer}>
+          <a
+            className={styles.linkBadge}
+            href="https://github.com/LeagueML"
+            target="_blank"
             rel="noreferrer"
           >
-            <BsGithub color='white' />
-            <span className={ styles.link }>GitHub</span>
+            <BsGithub color="white" />
+            <span className={styles.link}>GitHub</span>
           </a>
         </div>
-        <div className={ styles.linkContainer }>
-          <a 
-            className={ styles.linkBadge } 
-            href='https://api.league.ml/' 
-            target='_blank' 
+        <div className={styles.linkContainer}>
+          <a
+            className={styles.linkBadge}
+            href="https://api.league.ml/"
+            target="_blank"
             rel="noreferrer"
           >
-            <GrGraphQl color='white' />
-            <span className={ styles.link }>API</span>
+            <GrGraphQl color="white" />
+            <span className={styles.link}>API</span>
           </a>
         </div>
-        <div className={ styles.linkContainer }>
-          <a 
-            className={ styles.linkBadge } 
-            href='https://components.league.ml/' 
-            target='_blank' 
+        <div className={styles.linkContainer}>
+          <a
+            className={styles.linkBadge}
+            href="https://components.league.ml/"
+            target="_blank"
             rel="noreferrer"
           >
-            <SiStorybook color='white' />
-            <span className={ styles.link }>Components</span>
+            <SiStorybook color="white" />
+            <span className={styles.link}>Components</span>
           </a>
         </div>
       </div>
-      <div className={ styles.secondFooter }>
-        <span className={ styles.copyright }>Copyright © 2022 LEAGUE.ML</span>
+      <div className={styles.secondFooter}>
+        <span className={styles.copyright}>Copyright © 2022 LEAGUE.ML</span>
       </div>
     </div>
   );
-}
+};
 
 export default Home;
